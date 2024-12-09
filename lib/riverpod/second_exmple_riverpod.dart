a4bd424e97da9c473eaaa906ac5a14137acee7a5
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-12-09 20:06:56 +0200

    - Add Future RiverPod with apis

--- lib/riverpod/second_exmple_riverpod.dart
+++ lib/riverpod/second_exmple_riverpod.dart
@@ -1,10 +1,18 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod2/product_model/product_model.dart';
 
 final futureProvider = FutureProvider<int>(
   (ref) => getData(),
 );
 
+final futureProviderRemote = FutureProvider.autoDispose<List<Map<String, dynamic>>>(
+  (ref) => getProducts(),
+);
 Future<int> getData() async {
   await Future.delayed(
     const Duration(seconds: 2),
@@ -12,35 +20,80 @@ Future<int> getData() async {
   return 5;
 }
 
+Future<List<Map<String, dynamic>>> getProducts() async {
+  var response = await http.get(
+    Uri.parse('https://fakestoreapi.com/products/'),
+  );
+
+  // var getData = json.decode(response.body);
+  // (getData as List<Map<String, dynamic>>)
+  //     .map((product) => ProductModel.fromJson(product))
+  //     .toList();
+  var data = List<Map<String, dynamic>>.from(
+    json.decode(response.body),
+  );
+
+  return data;
+}
+
 class SecondExampleRiverpod extends ConsumerWidget {
   const SecondExampleRiverpod({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
-    var number = ref.watch(futureProvider);
+    var number = ref.watch(futureProviderRemote);
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          number.when(
-            data: (data) {
-              return Center(
-                child: Text(
-                  data.toString(),
-                ),
-              );
-            },
-            error: (error, trace) {
-              return Center(
-                child: Text(
-                  trace.toString(),
-                ),
-              );
-            },
-            loading: () => const CircularProgressIndicator(),
-          ),
-        ],
+      body: number.when(
+        data: (data) {
+          return Expanded(
+              child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, int index) => Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                        data[index]["image"],
+                                      ),
+                                      fit: BoxFit.cover)),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            SizedBox(
+                              width: MediaQuery.sizeOf(context).width * .6,
+                              child: Text(
+                                data[index]["title"],
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )));
+        },
+        error: (error, trace) {
+          return Center(
+            child: Text(
+              error.toString(),
+            ),
+          );
+        },
+        loading: () =>const Center(child:  CircularProgressIndicator()),
       ),
     );
   }
